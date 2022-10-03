16ca3406c68570472485747157ae04fd44ff414f
Bob Fornal <bob.fornal@leadingedje.com>
2022-10-03 12:17:50 -0400

    Visual Improvements and Locally Stored Data

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -11,13 +11,33 @@ import blank from '@core/constants/empty-structure.json';
 })
 export class StorageService {
 
+  storage: any = window.localStorage;
+  key: string = 'data-100Days';
+
   _structure: Structure = blank;
   structure: BehaviorSubject<Structure> = new BehaviorSubject<Structure>(this._structure);
 
-  constructor() { }
+  constructor() {
+    this.loadStructure();
+  }
 
   structureChange = (newStructure: Structure): void => {
     this._structure = { ...newStructure };
     this.structure.next(this._structure);
+    this.storeStructure(this._structure);
   };
+
+  storeStructure = (structure: Structure): void => {
+    this.storage.setItem(this.key, JSON.stringify(structure));
+  };
+
+  loadStructure = () => {
+    const dataString: string | null = this.storage.getItem(this.key);
+    if (dataString === null) return;
+
+    const data: Structure = JSON.parse(dataString);
+    this._structure = { ...data };
+    this.structure.next(this._structure);
+  };
+
 }
