4bfe904ec90dcf081441f73e9a26fd97d074a87b
Bob Fornal <bob.fornal@leadingedje.com>
2022-10-04 17:28:38 -0400

    App Component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-fdescribe('AppComponent', () => {
+import { AppMenuComponent } from '@shared/_spec-tools/components/menu.component.spec';
+
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+
+        AppMenuComponent
       ],
     }).compileComponents();
   });
