74ca695b1882a795bdc813cf1ec4faafcfb2ca02
Bob Fornal <bob.fornal@leadingedje.com>
2022-10-04 14:44:57 -0400

    Missing Merged Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { GoalsComponent } from './pages/goals/goals.component';
 
-import { MenuComponent } from './features/menu/menu.component';
-import { TitleComponent } from './shared/title/title.component';
+import { ImagesComponent } from './shared/images/images.component';
 import { ItemImageComponent } from './shared/item-image/item-image.component';
 
+import { MenuComponent } from './features/menu/menu.component';
+import { TitleComponent } from './shared/title/title.component';
+
 import { ModalModule } from '@shared/modal/modal.module';
+
 import { DragAndDropDirective } from './shared/drag-and-drop.directive';
 
 @NgModule({
@@ -26,7 +29,8 @@ import { DragAndDropDirective } from './shared/drag-and-drop.directive';
     MenuComponent,
     TitleComponent,
     ItemImageComponent,
-    DragAndDropDirective
+    DragAndDropDirective,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
