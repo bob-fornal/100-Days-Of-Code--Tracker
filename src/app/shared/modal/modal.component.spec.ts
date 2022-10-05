12d0f851a070114a83f7fc60ad5120d4b26a25c3
Bob Fornal <bob.fornal@leadingedje.com>
2022-10-05 15:17:04 -0400

    Modal Unit Test Coverage

--- src/app/shared/modal/modal.component.spec.ts
+++ src/app/shared/modal/modal.component.spec.ts
@@ -20,7 +20,64 @@ describe('ModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('expects "init" to setup', () => {
+    spyOn(component.doc.body, 'appendChild').and.stub();
+    spyOn(component['element'], 'addEventListener').and.stub();
+    spyOn(component['modalService'], 'add');
+
+    component.init();
+    expect(component.doc.body.appendChild).toHaveBeenCalledWith(component['element']);
+    expect(component['element'].addEventListener).toHaveBeenCalledWith('click', component.handleClick);
+    expect(component['modalService'].add).toHaveBeenCalled();
   });
+
+  it('expects "destroy" to cleanup', () => {
+    spyOn(component['modalService'], 'remove');
+    spyOn(component['element'], 'remove').and.stub();
+
+    component.destroy();
+    expect(component['modalService'].remove).toHaveBeenCalledWith('ID');
+    expect(component['element'].remove).toHaveBeenCalled();
+  });
+
+  it('expects "handleClick" to not close if class is not included', () => {
+    const element: any = {
+      target: {
+        className: 'NONE'
+      }
+    };
+    spyOn(component, 'close').and.stub();
+
+    component.handleClick(element);
+    expect(component.close).not.toHaveBeenCalled();
+  });
+
+  it('expects "handleClick" to close if the right class is included', () => {
+    const element: any = {
+      target: {
+        className: 'days-modal'
+      }
+    };
+    spyOn(component, 'close').and.stub();
+
+    component.handleClick(element);
+    expect(component.close).toHaveBeenCalled();
+  });
+
+  it('expects "open" to change the element and add class', () => {
+    spyOn(component.doc.body.classList, 'add').and.stub();
+
+    component.open();
+    expect(component['element'].style.display).toEqual('block');
+    expect(component.doc.body.classList.add).toHaveBeenCalledWith('days-modal-open');
+  });
+
+  it('expects "close" to change the element and remove class', () => {
+    spyOn(component.doc.body.classList, 'remove').and.stub();
+
+    component.close();
+    expect(component['element'].style.display).toEqual('none');
+    expect(component.doc.body.classList.remove).toHaveBeenCalledWith('days-modal-open');
+  });
+
 });
